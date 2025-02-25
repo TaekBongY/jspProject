7ed5d9cb7ee1df10cc77a7f50ead3e2d78b57d8d
mojio <mohae@kakao.com>
2025-02-25 11:55:23 +0900

    jsp_1

---
+++ src/main/java/com/kh/member/model/vo/Member.java
@@ -0,0 +1,22 @@
+package com.kh.member.model.vo;
+
+import java.sql.Date;
+
+public class Member {
+	private int USER_NO;
+	private String USER_ID;
+	private String USER_PWD;
+	private String USER_NAME;
+	private String PHONE;
+	private String EMAIL;
+	private String ADDRESS;
+	private String INTEREST;
+	private Date ENROLL_DATE;
+	private Date MODIFY_DATE;
+	private String STATUS;
+	
+	public Member() {
+		super();
+	}
+	
+}
